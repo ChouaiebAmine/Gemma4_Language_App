84d5f7fa3ec056e1b518579ae3b355d993434984
unknown <chouaiebmedamine@gmail.com>
2026-05-10 11:26:18 +0100

    Modification: add auth route

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,7 @@ from routes.achievements import router as achievements_router
 from routes.languages import router as languages_router
 from routes.analytics import router as analytics_router
 from routes.recommend import router as recommend_router
+from routes.auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from database import load_languages
@@ -49,3 +50,4 @@ app.include_router(achievements_router)
 app.include_router(languages_router)
 app.include_router(analytics_router)
 app.include_router(recommend_router)
+app.include_router(auth_router)
